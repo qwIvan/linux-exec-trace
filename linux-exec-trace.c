6616206e0e29f788aadbd8f1f3505f863aef6316
jjqq <osexp2000@gmail.com>
2017-06-15 21:22:00 +0900

    t

--- linux-exec-trace.c
+++ linux-exec-trace.c
@@ -1,60 +1,3 @@
-/* extrace - trace exec() calls system-wide
- *
- * Requires CONFIG_CONNECTOR=y and CONFIG_PROC_EVENTS=y.
- * Requires root or "setcap cap_net_admin+ep extrace".
- *
- * Usage: extrace [-deflq] [-o FILE] [-p PID|CMD...]
- * default: show all exec(), globally
- * -p PID   only show exec() descendant of PID
- * CMD...   run CMD... and only show exec() descendant of it
- * -o FILE  log to FILE instead of standard output
- * -d       print cwd of process
- * -e       print environment of process
- * -q       don't print exec() arguments
- *
- * Copyright (C) 2014-2016 Leah Neukirchen <leah@vuxu.org>
- *
- * hacked from sources of:
- */
-/* exec-notify, so you can watch your acrobat reader or vim executing "bash -c"
- * commands ;-)
- * Requires some 2.6.x Linux kernel with proc connector enabled.
- *
- * $  cc -Wall -ansi -pedantic -std=c99 exec-notify.c
- *
- * (C) 2007-2010 Sebastian Krahmer <krahmer@suse.de> original netlink handling
- * stolen from an proc-connector example, copyright folows:
- */
-/* Copyright (C) Matt Helsley, IBM Corp. 2005
- * Derived from fcctl.c by Guillaume Thouvenin
- * Original copyright notice follows:
- *
- * Copyright (C) 2005 BULL SA.
- * Written by Guillaume Thouvenin <guillaume.thouvenin@bull.net>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
-
-#define _XOPEN_SOURCE 700
-
-//todo: show cmdline in multiple lines
-//todo: read keyboard to enable/disable logging CWD / ENV / argv ...
-//todo: log file event
-//todo: log fork and process exit event
-//todo: containerization
-
 #include <fcntl.h>
 #include <limits.h>
 #include <signal.h>
@@ -71,8 +14,8 @@
 #include <linux/netlink.h>
 #include <linux/cn_proc.h>
 
-#define max(x,y) ((y)<(x)?(x):(y))
-#define min(x,y) ((y)>(x)?(x):(y))
+#define max(x, y) ((y)<(x)?(x):(y))
+#define min(x, y) ((y)>(x)?(x):(y))
 
 #define SEND_MESSAGE_LEN (NLMSG_LENGTH(sizeof (struct cn_msg) + \
                                        sizeof (enum proc_cn_mcast_op)))
@@ -88,7 +31,7 @@
 #define CMDLINE_MAX_CHARS      (65536 - sizeof(TRUNCATED_S))
 char cmdline[CMDLINE_MAX_CHARS + sizeof(TRUNCATED_S)] = {0};
 
-pid_t parent = 1;
+pid_t rootPid = 1;
 int show_args = 1;
 int show_cwd = 0;
 int show_env = 0;
@@ -96,23 +39,20 @@ FILE *output;
 sig_atomic_t quit = 0;
 
 static void
-sigint(int sig)
-{
-  (void)sig;
+sigint(int sig) {
+  (void) sig;
   quit = 1;
 }
 
 static void
-sigchld(int sig)
-{
-  (void)sig;
-  while (waitpid(-1, NULL, WNOHANG) > 0)
-    ;
+sigchld(int sig) {
+  (void) sig;
+  while (waitpid(-1, NULL, WNOHANG) > 0);
   quit = 1;
 }
 
-static void _print_shquoted(const char *s, const char *meta_chars) {
-  if (*s && !strpbrk(s, meta_chars) {
+static void _printQuoted(const char *s, const char *meta_chars) {
+  if (*s && !strpbrk(s, meta_chars)) {
     fprintf(output, "%s", s);
     return;
   }
@@ -124,175 +64,189 @@ static void _print_shquoted(const char *s, const char *meta_chars) {
     else if (*s == '\n')
       fprintf(output, "'$'\\n''");
     else
+      //todo: need more escape
       putc(*s, output);
   putc('\'', output);
 }
 
-static void print_shquoted(const char *s) {
-  print_shquoted(s, "\001\002\003\004\005\006\007\010"
-      "\011\012\013\014\015\016\017\020"
-      "\021\022\023\024\025\026\027\030"
-      "\031\032\033\034\035\036\037\040"
+static void printQuoted(const char *s) {
+  printQuoted(s, "\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017\020"
+      "\021\022\023\024\025\026\027\030\031\032\033\034\035\036\037\040"
       "`^#*[]|\\?${}()'\"<>&;\177");
 }
 
-static void print_shquoted_env(const char *s) {
-  print_shquoted(s, "\001\002\003\004\005\006\007\010"
-      "\011\012\013\014\015\016\017\020"
-      "\021\022\023\024\025\026\027\030"
-      "\031\032\033\034\035\036\037\040"
+static void printQuoted_env(const char *s) {
+  printQuoted(s, "\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017\020"
+      "\021\022\023\024\025\026\027\030\031\032\033\034\035\036\037\040"
       "`^#*[]|\\?${}()'\"<>&;\177=");
 }
 
-static void
-handle_msg(struct cn_msg *cn_hdr)
-{
-  char name[PATH_MAX];
-  char exe[PATH_MAX];
-  char cwd[PATH_MAX];
-  char *argvrest;
-  char ppid_s[16] = {0};
-  char uid_s[16] = {0};
-  char gid_s[16] = {0};
+static bool isDescendantPid(pid_t pid, pid_t rootPid) {
+  char proc_path[PATH_MAX];
+  snprintf(proc_path, sizeof proc_path, "/proc/%d/stat", pid);
 
-  int r = 0, r2 = 0, d;
-  struct proc_event *ev = (struct proc_event *)cn_hdr->data;
+  pid_t ppid = 0;
+  FILE *f = fopen(proc_path, "r");
+  if (f != NULL) {
+    if (fscanf(f, "%*d (%*[^)]) %*c %d", &ppid) < 0) {
+      fclose(f);
+      return false;
+    }
+    fclose(f);
+  } else {
+    return false;
+  }
+
+  if (ppid == rootPid)
+    return true;
+
+  return isDescendantPid(ppid, rootPid);
+}
+
+static void
+handle_msg(struct cn_msg *cn_hdr) {
+  char proc_path[PATH_MAX];
+
+  struct proc_event *ev = (struct proc_event *) cn_hdr->data;
+  if (ev->what != PROC_EVENT_EXEC) return;
+
   pid_t pid = ev->event_data.exec.process_tgid;
 
-  if (ev->what == PROC_EVENT_EXEC) {
+  if (rootPid != 1 && !isDescendantPid(pid, rootPid))
+    return;
 
-    snprintf(name, sizeof name, "/proc/%d/cmdline", pid);
-    {
-      int fd = open(name, O_RDONLY);
-      if (fd > 0) {
-        r = read(fd, cmdline, CMDLINE_MAX_CHARS + 2/*for overflow test*/);
-        close(fd);
-
-        //if /proc/PID/cmdline is overflow, then join it with "... <truncated>"
-        if (r == CMDLINE_MAX_CHARS + 2) {
-          strcpy(cmdline + CMDLINE_MAX_CHARS, TRUNCATED_S);
-          r = sizeof cmdline;
-        } else if (r > 0) {
-          cmdline[r] = 0;
-        } else {
-          r = 0;
-          cmdline[0] = 0;
+  char ppid_s[16] = "?";
+  char uid_s[16] = "?";
+  char gid_s[16] = "?";
+  snprintf(proc_path, sizeof proc_path, "/proc/%d/status", pid);
+  {
+    FILE *f = fopen(proc_path, "r");
+    if (f != nil) {
+      char *line = 0, *eq = 0;
+      size_t lineLen = 0;
+      while (getline(&line, &lineLen, f) >= 0) {
+        if (sscanf(line, "PPid:\t%10s", ppid_s) == 1) {
+        } else if (sscanf(line, "Uid:\t%10s", uid_s) == 1) {
+        } else if (sscanf(line, "Gid:\t%10s", gid_s) == 1) {
+          break;
         }
-
-        argvrest = strchr(cmdline, 0) + 1;
       }
-      else {
-        r = 0;
-        cmdline[0] = 0;
-      }
+      free(line);
+      fclose(f);
     }
+  }
 
-    snprintf(name, sizeof name, "/proc/%d/exe", pid);
+  {
+    char *comm = NULL;
+    snprintf(proc_path, sizeof proc_path, "/proc/%d/comm", pid);
     {
-      int len = readlink(name, exe, sizeof(exe)-1);
-      if (len > 0)
-        exe[len] = 0;
-      else
-        exe[0] = 0;
-    }
-
-    if (show_cwd) {
-      snprintf(name, sizeof name, "/proc/%d/cwd", pid);
-      int len = readlink(name, cwd, sizeof(cwd)-1);
-      if (len > 0)
-        cwd[len] = 0;
-      else
-        cwd[0] = 0;
-    }
-
-    strcpy(ppid_s, "?");
-    strcpy(uid_s, "?");
-    strcpy(gid_s, "?");
-    snprintf(name, sizeof name, "/proc/%d/status", pid);
-    {
-      FILE *f;
-      if ((f = fopen(name, "r"))) {
-        char *line = 0, *eq = 0;
-        size_t lineLen = 0;
-        while (getline(&line, &lineLen, f) >= 0) {
-          if (sscanf(line, "PPid:\t%10s", ppid_s)==1) {
-          } else if (sscanf(line, "Uid:\t%10s", uid_s)==1) {
-          } else if (sscanf(line, "Gid:\t%10s", gid_s)==1) {
-            break;
-          }
-        }
-        free(line);
+      FILE *f = fopen(proc_path, "r");
+      if (f != nil) {
+        size_t size = 0;
+        getline(&comm, &size, f);
         fclose(f);
       }
     }
 
+    fprintf(output, "%d %s %s:%s %s", pid, ppid_s, uid_s, gid_s, comm == NULL ? "" : comm);
 
-    fprintf(output, "%d %s %s:%s ", pid, ppid_s, uid_s, gid_s);
-
-    if (show_cwd) {
-      print_shquoted(cwd);
-      fprintf(output, " %% ");
-    }
-
-    print_shquoted(exe);
-    if (strcmp(exe, cmdline)) {
-      putc('(', output);
-      print_shquoted(cmdline);
-      putc(')', output);
-    }
-
-    if (show_args && r > 0) {
-      while (argvrest - cmdline < r) {
-        putc(' ', output);
-        print_shquoted(argvrest);
-        argvrest = strchr(argvrest, 0)+1;
-      }
-    }
-
-    if (show_env) {
-      FILE *env;
-      fprintf(output, "  ");
-      snprintf(name, sizeof name, "/proc/%d/environ", pid);
-      if ((env = fopen(name, "r"))) {
-        char *line = 0, *eq = 0;
-        size_t linelen = 0;
-        while (getdelim(&line, &linelen, '\0', env) >= 0) {
-          putc(' ', output);
-          if ((eq = strchr(line, '='))) {
-            /* print split so = doesn't trigger escaping.  */
-            *eq = 0;
-            print_shquoted_env(line);
-            putc('=', output);
-            print_shquoted_env(eq+1);
-          } else {
-            /* weird env entry without equal sign.  */
-            print_shquoted_env(line);
-          }
-        }
-        free(line);
-        fclose(env);
-      } else {
-        fprintf(output, " -");
-      }
-    }
-
-    fprintf(output, "\n");
-    fflush(output);
+    free(comm);
   }
+
+  {
+    snprintf(proc_path, sizeof proc_path, "/proc/%d/cwd", pid);
+    char cwd[PATH_MAX] = "";
+    int len = readlink(proc_path, cwd, sizeof(cwd) - 1);
+    if (len > 0)
+      cwd[len] = 0;
+    fprintf(output, "    cd ");
+    printQuoted(cwd);
+    fprintf(output, " & \\\n");
+  }
+
+  if (show_env) {
+    snprintf(proc_path, sizeof proc_path, "/proc/%d/environ", pid);
+    FILE* f = fopen(proc_path, "r");
+    if (f != NULL) {
+      char *kv = 0, *eq = 0;
+      size_t size = 0;
+      while (getdelim(&kv, &size, '\0', env) >= 0) {
+        fprintf(output, "     ");
+        if ((eq = strchr(kv, '='))) {
+          *eq = 0;
+          printQuoted_env(kv);
+          fprintf(output, "=");
+          printQuoted_env(eq + 1);
+        } else {
+          printQuoted_env(kv);
+          fprintf(output, "=");
+        }
+        fprintf(output, " \\\n");
+      }
+      free(kv);
+      fclose(env);
+    } else {
+      fprintf(output, " -");
+    }
+  }
+
+  char exe[PATH_MAX] = "";
+  snprintf(proc_path, sizeof proc_path, "/proc/%d/exe", pid);
+  {
+    int len = readlink(proc_path, exe, sizeof(exe) - 1);
+    if (len > 0)
+      exe[len] = 0;
+  }
+
+  snprintf(proc_path, sizeof proc_path, "/proc/%d/cmdline", pid);
+  {
+    FILE* f = fopen(proc_path, "r");
+    char* arg = NULL;
+    size_t size = 0;
+    int count = 0;
+    while (getdelim(&arg, &size, '\0', cmdline) != -1) {
+      if (count == 0) {
+        if (arg[0] == '/' || (arg[0] == '.' && arg[0] == '/') || (arg[0] == '..' && arg[0] == '/')) {
+          fprintf(output, "$___ ");
+          printQuoted(exe);
+          fprintf(output, " \\\n");
+        } else {
+          fprintf(output, "$___ exec -a ");
+          printQuoted(arg);
+          fprintf(output, " ");
+          printQuoted(exe);
+          fprintf(output, " \\\n");
+        }
+      } else {
+        fprintf(output, "$___    ");
+        printQuoted(arg);
+      }
+      fprintf(output, " \\\n");
+      count++;
+    }
+    free(arg);
+    fclose(f);
+
+    if (count == 0) {
+      printQuoted(exe);
+    }
+  }
+  fprintf(output, "\n");
+
+  fflush(output);
 }
 
 const char my_short_opts[] = "efht";
 struct option my_long_opts[] = {
-    {"exec", 0, NULL, 'e'},
-    {"fork", 0, NULL, 'f'},
-    {"help", 0, NULL, 'h'},
+    {"exec",   0, NULL, 'e'},
+    {"fork",   0, NULL, 'f'},
+    {"help",   0, NULL, 'h'},
     {"thread", 0, NULL, 't'},
     {}
 };
 
 int
-main(int argc, char *argv[])
-{
+main(int argc, char *argv[]) {
   int sk_nl;
   struct sockaddr_nl my_nla, kern_nla, from_nla;
   socklen_t from_nla_len;
@@ -303,32 +257,44 @@ main(int argc, char *argv[])
   size_t recv_len = 0;
   int rc = -1, opt;
 
-  const char* exeName = strrchr(argv[0], '/');
+  const char *exeName = strrchr(argv[0], '/');
   if (!exeName) exeName = argv[0];
 
   output = stdout;
 
   while ((opt = getopt(argc, argv, "+delfo:p:qw")) != -1)
     switch (opt) {
-    case 'd': show_cwd = 1; break;
-    case 'e': show_env = 1; break;
-    case 'l': /* obsoleted, ignore */; break;
-    case 'f': /* obsoleted, ignore */; break;
-    case 'p': parent = atoi(optarg); break;
-    case 'q': show_args = 0; break;
-    case 'o':
-      output = fopen(optarg, "w");
-      if (!output) {
-        perror("fopen");
-        exit(1);
-      }
-      break;
-    case 'w': /* obsoleted, ignore */; break;
-    default: goto usage;
+      case 'd':
+        show_cwd = 1;
+        break;
+      case 'e':
+        show_env = 1;
+        break;
+      case 'l': /* obsoleted, ignore */;
+        break;
+      case 'f': /* obsoleted, ignore */;
+        break;
+      case 'p':
+        rootPid = atoi(optarg);
+        break;
+      case 'q':
+        show_args = 0;
+        break;
+      case 'o':
+        output = fopen(optarg, "w");
+        if (!output) {
+          perror("fopen");
+          exit(1);
+        }
+        break;
+      case 'w': /* obsoleted, ignore */;
+        break;
+      default:
+        goto usage;
     }
 
-  if (parent != 1 && optind != argc) {
-usage:
+  if (rootPid != 1 && optind != argc) {
+    usage:
     fprintf(stderr, "Usage: %s [-d|--cwd] [-e|--env] [-q|--no-args]\n", exeName);
     fprintf(stderr, "       %.*s [-o FILE] [-p PID|CMD...]\n", strlen(exeName));
     exit(1);
@@ -336,7 +302,7 @@ usage:
 
   sk_nl = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_CONNECTOR);
   if (sk_nl == -1) {
-    perror("socket sk_nl error");
+    perror("socket NETLINK_CONNECTOR");
     exit(1);
   }
 
@@ -348,13 +314,13 @@ usage:
   kern_nla.nl_groups = CN_IDX_PROC;
   kern_nla.nl_pid = 1;
 
-  if (bind(sk_nl, (struct sockaddr *)&my_nla, sizeof my_nla) == -1) {
-    perror("binding sk_nl error");
+  if (bind(sk_nl, (struct sockaddr *) &my_nla, sizeof my_nla) == -1) {
+    perror("bind CN_IDX_PROC");
     goto close_and_exit;
   }
-  nl_hdr = (struct nlmsghdr *)buff;
-  cn_hdr = (struct cn_msg *)NLMSG_DATA(nl_hdr);
-  mcop_msg = (enum proc_cn_mcast_op*)&cn_hdr->data[0];
+  nl_hdr = (struct nlmsghdr *) buff;
+  cn_hdr = (struct cn_msg *) NLMSG_DATA(nl_hdr);
+  mcop_msg = (enum proc_cn_mcast_op *) &cn_hdr->data[0];
 
   memset(buff, 0, sizeof buff);
   *mcop_msg = PROC_CN_MCAST_LISTEN;
@@ -369,10 +335,10 @@ usage:
   cn_hdr->id.val = CN_VAL_PROC;
   cn_hdr->seq = 0;
   cn_hdr->ack = 0;
-  cn_hdr->len = sizeof (enum proc_cn_mcast_op);
+  cn_hdr->len = sizeof(enum proc_cn_mcast_op);
 
   if (send(sk_nl, nl_hdr, nl_hdr->nlmsg_len, 0) != nl_hdr->nlmsg_len) {
-    perror("failed to send proc connector mcast ctl op!");
+    perror("send");
     goto close_and_exit;
   }
 
@@ -384,7 +350,7 @@ usage:
   if (optind != argc) {
     pid_t child;
 
-    parent = getpid();
+    rootPid = getpid();
     signal(SIGCHLD, sigchld);
 
     child = fork();
@@ -393,7 +359,7 @@ usage:
       goto close_and_exit;
     }
     if (child == 0) {
-      execvp(argv[optind], argv+optind);
+      execvp(argv[optind], argv + optind);
       perror("execvp");
       goto close_and_exit;
     }
@@ -405,10 +371,10 @@ usage:
   while (!quit) {
     memset(buff, 0, sizeof buff);
     from_nla_len = sizeof from_nla;
-    nlh = (struct nlmsghdr *)buff;
+    nlh = (struct nlmsghdr *) buff;
     memcpy(&from_nla, &kern_nla, sizeof from_nla);
     recv_len = recvfrom(sk_nl, buff, BUFF_SIZE, 0,
-                        (struct sockaddr *)&from_nla, &from_nla_len);
+                        (struct sockaddr *) &from_nla, &from_nla_len);
     if (from_nla.nl_pid != 0 || recv_len < 1)
       continue;
 
@@ -426,7 +392,7 @@ usage:
     }
   }
 
-close_and_exit:
+  close_and_exit:
   close(sk_nl);
   return rc;
 }
